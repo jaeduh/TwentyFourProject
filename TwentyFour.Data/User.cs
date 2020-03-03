4c8dc2ebe8f80928b29456a511c24283ec93ce32
Jaeduh <jada.g1210@gmail.com>
2020-03-03 14:46:57 -0500

    ReadyForMerge

--- TwentyFour.Data/User.cs
+++ TwentyFour.Data/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace TwentyFour.Data
 {
     public class User
     {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string testProperty { get; set; }
     }
 }
