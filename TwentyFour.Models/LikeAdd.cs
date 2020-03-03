3a6ecc33065362dfa56ff558ac284bdc43e7e7f7
Jess Schultz <schultz.jess.a@outlook.com>
2020-03-03 10:21:20 -0600

    Added Like frame

--- TwentyFour.Models/LikeAdd.cs
+++ TwentyFour.Models/LikeAdd.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwentyFour.Data;
 
-namespace TwentyFour.Data
+namespace TwentyFour.Models
 {
-    public class Like
+    class LikeAdd
     {
-        [Key]
-        public int Id { get; set; }
         public int LikeCount { get; set; }
 
         public Post Liked { get; set; }
--- TwentyFour.Services/LikeServices.cs
+++ TwentyFour.Services/LikeServices.cs
@@ -8,5 +8,20 @@ namespace TwentyFour.Services
 {
     public class LikeServices
     {
+        public bool AddLike(LikeModel model)
+        {
+            var like = new Like()
+            {
+                Liker = model.Liker,
+                Liked = model.Liked,
+            };
+            using (var context = new ApplicationDbContext())
+            {
+                context.Like.Add(like);
+                return context.SaveChanges() == 1;
+            }
+
+        }
+
     }
 }
