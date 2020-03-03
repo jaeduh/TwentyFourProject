cbfad00711fd41120f812c516f94a3a6f83071d2
craig dunlap <cadunlap3234@gmail.com>
2020-03-03 11:07:46 -0500

    post method

--- TwentyFour.Data/Post.cs
+++ TwentyFour.Data/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace TwentyFour.Data
 {
     public class Post
     {
+        [Key]
+        public string PostId { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+
+        [MaxLength(8000)]
+        public string Text { get; set; }
+        public User Author { get; set; }
     }
 }
--- TwentyFour.Services/PostServices.cs
+++ TwentyFour.Services/PostServices.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwentyFour.Data;
+using TwentyFourProject.Data;
 
 namespace TwentyFour.Services
 {
     public class PostServices
     {
+        private readonly Guid _CustomerId;
+
+        public PostServices(Guid userId)
+        {
+            _CustomerId = userId;
+        }
+
+        public bool CreatePost(PostServices post)
+        {
+            var entity =
+                new Post()
+                {
+                    Id = _CustomerId,
+                    Title = post.Title,
+                    Text = post.Text
+                };
+
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<PostItem> GetPost()
+        {
+            using (var ctx = new ApplicationDbContext))
+                {
+                var query =
+                    ctx
+
+                        .Posts
+                        .Where(e => e.CustomerId == _CustomerId)
+                        .Select(
+                        e =>
+                        new PostItem
+                        {
+                            PostId = e.PostId,
+                            Title = e.Title
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
     }
 }
